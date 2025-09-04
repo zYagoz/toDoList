594ddcf1b8125ce23b045d2687e47857853e964b
zYagoz <yagoapp2013@gmail.com>
2025-09-04 20:39:21 -0300

    feat: adiciona models e controller de lista, ajusta rotas e index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'node:path'
 import {fileURLToPath} from 'node:url'
+import router from './routes.js';
 
 const file = fileURLToPath(import.meta.url);
 const dir = path.dirname(file)
@@ -13,7 +14,9 @@ app.set('views', path.join(dir, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 
-const PORT = process.env.PORT ||  3000;
+app.use(router)
+
+const PORT = 3000;
 app.listen(PORT, () =>{
     console.log(`Servido Iniciado \nRodando em http://localhost:${PORT}`)
 })
---
+++ src/models/listModel.js
@@ -0,0 +1,65 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class ListUserCreate {
+    constructor(name){
+        this.id = uuidv4();
+        this.name = name;
+        this.status = incompleted;
+        this.createdAt = new Date();
+        this.updateAt = new Date();
+        this.tasks = [];
+    }
+}
+
+let listCreated = {};
+
+export const listModel = {
+    getAllList() {
+        return listCreated
+    },
+
+    getListById(id){
+        for(let key of listCreated){
+            if(key === id){
+                return key
+            }
+        }
+    },
+
+    createList(name){
+        const list = new ListUserCreate(name);
+        return list
+        
+    },
+
+    saveList(list){
+        listCreated[list.id] = list;
+    },
+
+    updateList(id, newName){
+        for(let key of listCreated){
+            if(key === id){
+                key.updateAt = new Date();
+                key.name = newName;
+                return key
+            }
+        }
+    },
+
+    changeStatusList(id){
+        if(listCreated[id] === incompleted){
+            listCreated[id].status = completed
+        } else{
+            listCreated[id].status = incompleted
+        };  
+    },
+
+    deleteList(id){
+        if(listCreated[id]){
+            delete listCreated[id];
+            return true
+        }
+        return false
+    }
+
+}
